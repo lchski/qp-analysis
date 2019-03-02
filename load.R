ee7e73277f46a675dd486331caf68dcda4145206
Lucas Cherkewski <lucas@ecustom.ca>
2019-03-02 13:41:10 -0500

    more stuff!

--- load.R
+++ load.R
@@ -1,9 +1,17 @@
 library(tidyverse)
 library(textclean)
+library(tidytext)
+library(SnowballC)
+
+data("stop_words")
+
+stop_words <- stop_words %>% add_row(word = "speaker", lexicon = "custom")
 
 library(RPostgreSQL)
 library(DBI)
 library(dbplyr)
+
+library(tm)
 
 con <- DBI::dbConnect(
     drv = dbDriver("PostgreSQL"),
@@ -12,15 +20,31 @@ con <- DBI::dbConnect(
   )
 
 ## Pull all QP statements (those labelled by a string containing "Oral", to capture variations)
-ethi_statements <- tbl(con, "hansards_statement") %>%
+qp_statements <- tbl(con, "hansards_statement") %>%
   filter(str_detect(h1_en, "Oral")) %>%
   collect() %>%
   mutate(
     content_en_plaintext = replace_html(content_en)
-  )
+  ) %>%
+  arrange(id)
 
 ## Pull all MPs, and information about their parties
 mps <- tbl(con, "core_electedmember") %>%
   right_join(tbl(con, "core_party"), by = c("party_id" = "id")) %>%
   right_join(tbl(con, "core_politician"), by = c("politician_id" = "id")) %>%
   collect()
+
+sampleData <- qp_statements %>% top_n(1000, id)
+
+tinyData <- sampleData %>%
+  select(id, time, h2_en, who_en, content_en_plaintext) %>%
+  filter(str_detect(who_en, "Bardish")) %>%
+  unnest_tokens(word, content_en_plaintext) %>%
+  anti_join(stop_words) %>%
+  mutate(word = wordStem(word))
+## TODO: Compare with word stemming vs not
+
+tinyData %>%
+  group_by(word) %>%
+  summarize(count = n()) %>%
+  arrange(-count)

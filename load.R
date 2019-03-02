e1d8889e6be7571d770a1608fdd17acbe8866b7e
Lucas Cherkewski <lucas@ecustom.ca>
2019-03-02 15:37:48 -0500

    separate file into two

--- load.R
+++ load.R
@@ -1,12 +1,5 @@
 library(tidyverse)
 library(textclean)
-library(tidytext)
-library(SnowballC)
-library(exploratory)
-
-data("stop_words")
-
-stop_words <- stop_words %>% add_row(word = "speaker", lexicon = "custom")
 
 library(RPostgreSQL)
 library(DBI)
@@ -32,37 +25,3 @@ mps <- tbl(con, "core_electedmember") %>%
   right_join(tbl(con, "core_party"), by = c("party_id" = "id")) %>%
   right_join(tbl(con, "core_politician"), by = c("politician_id" = "id")) %>%
   collect()
-
-sampleData <- qp_statements %>% top_n(1000, id)
-
-tinyData <- qp_statements %>%
-  filter(! procedural) %>%
-  filter(! is.na(politician_id)) %>%
-  filter(time > "2018-01-01") %>%
-  select(id, time, h2_en, who_en, content_en_plaintext) %>%
-  unnest_tokens(word, content_en_plaintext) %>%
-  anti_join(stop_words) %>%
-  mutate(word = wordStem(word))
-## TODO: Compare with word stemming vs not
-
-statements_by_word_frequency <- tinyData %>%
-  count(id, word, sort = TRUE) %>%
-  ungroup() %>%
-  bind_tf_idf(word, id, n)
-
-statement_similarity <- statements_by_word_frequency %>%
-  arrange(tf_idf) %>%
-  left_join(qp_statements) %>%
-  select(id:tf_idf, time, h2_en, who_en) %>%
-  do_cosine_sim.kv(subject = id, key = word, value = tf_idf, distinct = TRUE)
-
-top_similar_statements <- statement_similarity %>%
-  filter(value > 0.5) %>%
-  arrange(desc(value)) %>%
-  top_n(100) %>%
-  select(id.x, id.y)
-
-qp_statements %>%
-  select(id, time, h2_en, who_en, content_en_plaintext) %>%
-  filter(id %in% top_similar_statements$id.x | id %in% top_similar_statements$id.y) %>%
-  View()
---
+++ similarity.R
@@ -0,0 +1,48 @@
+# SETUP
+source("load.R")
+
+library(tidytext)
+library(SnowballC)
+library(exploratory)
+
+data("stop_words")
+stop_words <- stop_words %>% add_row(word = "speaker", lexicon = "custom")
+
+
+# ANALYSIS
+
+## Load the statements we want to work with.
+## NB: Change the filter as you see fit.
+statements_to_analyse <- qp_statements %>%
+  filter(time > "2018-01-01")
+
+## Reduce the statements to lemmatized tokens. (Thanks Busa!)
+statements_by_token <- statements_to_analyse %>%
+  filter(! procedural) %>%
+  filter(! is.na(politician_id)) %>%
+  select(id, content_en_plaintext) %>%
+  unnest_tokens(word, content_en_plaintext) %>%
+  anti_join(stop_words) %>%
+  mutate(word = wordStem(word))
+
+## Calculate the TF-IDF score for each word.
+statements_by_word_frequency <- statements_by_token %>%
+  count(id, word, sort = TRUE) %>%
+  ungroup() %>%
+  bind_tf_idf(word, id, n)
+
+## Calculate the cosine similarity of each QP statement.
+statement_similarity <- statements_by_word_frequency %>%
+  do_cosine_sim.kv(subject = id, key = word, value = tf_idf, distinct = TRUE)
+
+## Retrieve the statements with the highest similarity scores.
+top_similar_statements <- statement_similarity %>%
+  filter(value > 0.5) %>%
+  top_n(-100, value) %>%
+  select(id.x, id.y)
+
+## View the statements with the highest scores.
+qp_statements %>%
+  select(id, time, h2_en, who_en, content_en_plaintext) %>%
+  filter(id %in% top_similar_statements$id.x | id %in% top_similar_statements$id.y) %>%
+  View()

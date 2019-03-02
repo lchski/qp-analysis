5cf0de658623a0f5e8a574c2c4eef3d157d75793
Lucas Cherkewski <lucas@ecustom.ca>
2019-03-02 12:06:03 -0500

    heyo!

---
+++ load.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(textclean)
+
+library(RPostgreSQL)
+library(DBI)
+library(dbplyr)
+
+con <- DBI::dbConnect(
+    drv = dbDriver("PostgreSQL"),
+    host = "localhost",
+    dbname = "openparliament"
+  )
+
+## Pull all QP statements (those labelled by a string containing "Oral", to capture variations)
+ethi_statements <- tbl(con, "hansards_statement") %>%
+  filter(str_detect(h1_en, "Oral")) %>%
+  collect() %>%
+  mutate(
+    content_en_plaintext = replace_html(content_en)
+  )
+
+## Pull all MPs, and information about their parties
+mps <- tbl(con, "core_electedmember") %>%
+  right_join(tbl(con, "core_party"), by = c("party_id" = "id")) %>%
+  right_join(tbl(con, "core_politician"), by = c("politician_id" = "id")) %>%
+  collect()

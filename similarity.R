2b62227917081636e8277f217dc80ec65dd031a4
Lucas Cherkewski <lucas@ecustom.ca>
2019-03-17 19:34:09 -0400

    remove now-unused clustering experiments

--- similarity.R
+++ similarity.R
@@ -28,54 +28,6 @@ base_statements <- qp_statements %>%
 
 
 ## CLUSTERING
-
-### Start with statements that have some similarity.
-cluster_statements <- statement_similarity %>%
-  filter(value > 0.5)
-cluster_statement_ids <- union(cluster_statements$id.x, cluster_statements$id.y)
-
-### Get the statement full text.
-cluster_statements_to_analyse <- statements_to_analyse %>%
-  filter(id %in% cluster_statement_ids)
-
-### Reduce the statements to lemmatized tokens. (Thanks Busa!)
-cluster_statements_by_token <- cluster_statements_to_analyse %>%
-  select(id, content_en_plaintext) %>%
-  unnest_tokens(word, content_en_plaintext) %>%
-  anti_join(stop_words) %>%
-  mutate(word = wordStem(word))
-
-### Calculate the TF-IDF score for each word.
-cluster_statements_by_word_frequency <- cluster_statements_by_token %>%
-  count(id, word, sort = TRUE) %>%
-  ungroup() %>%
-  bind_tf_idf(word, id, n)
-
-cluster_statements_with_reduced_dimensions <- cluster_statements_by_word_frequency %>%
-  do_svd.kv(id, word, tf_idf, n_component = 3)
-
-cluster_statements_with_reduced_dimensions_spread <- cluster_statements_with_reduced_dimensions %>%
-  spread(new.dimension, value)
-
-optimal_cluster_count <- NbClust(cluster_statements_with_reduced_dimensions_spread, method = "complete")
-
-clusters2 <- kmeans(cluster_statements_with_reduced_dimensions_spread, centers = 2, nstart = 25)
-clusters3 <- kmeans(cluster_statements_with_reduced_dimensions_spread, centers = 3, nstart = 25)
-clusters7 <- kmeans(cluster_statements_with_reduced_dimensions_spread, centers = 7, nstart = 25)
-clusters12 <- kmeans(cluster_statements_with_reduced_dimensions_spread, centers = 12, nstart = 25)
-
-fviz_nbclust(cluster_statements_with_reduced_dimensions_spread, kmeans, method = "wss")
-
-p1 <- fviz_cluster(clusters2, geom = "point", data = cluster_statements_with_reduced_dimensions_spread) + ggtitle("k = 2")
-p2 <- fviz_cluster(clusters3, geom = "point",  data = cluster_statements_with_reduced_dimensions_spread) + ggtitle("k = 3")
-p3 <- fviz_cluster(clusters7, geom = "point",  data = cluster_statements_with_reduced_dimensions_spread) + ggtitle("k = 7")
-p4 <- fviz_cluster(clusters12, geom = "point",  data = cluster_statements_with_reduced_dimensions_spread) + ggtitle("k = 12")
-
-library(gridExtra)
-grid.arrange(p1, p2, p3, p4, nrow = 2)
-
-
-
 target_ids <- analyse_statement_similarity(
   statements = base_statements %>%
     filter(slug.x == "liberal") %>%
@@ -86,39 +38,10 @@ target_ids <- analyse_statement_similarity(
 target_statements <- base_statements %>%
   filter(id %in% target_ids)
 
-cluster_statements_with_reduced_dimensions <- analyse_statement_similarity(
-  statements = target_statements,
-  similarity_threshold = 0
-) %>%
-  extract2("token_frequency") %>%
-  do_svd.kv(id, word, tf_idf, n_component = 3)
-  
-cluster_statements_with_reduced_dimensions_spread <- cluster_statements_with_reduced_dimensions %>%
-  spread(new.dimension, value)
-
-optimal_cluster_count <- NbClust(cluster_statements_with_reduced_dimensions_spread, method = "complete")
-
-
 test <- cluster_statements_kmeans(target_statements)
+
 fviz_cluster(test$clusters, geom = "point", data = test$scored_statements_by_id) +
   ggtitle(paste0("k = ", test$cluster_count))
-
-
-clusters2 <- kmeans(cluster_statements_with_reduced_dimensions_spread, centers = 2, nstart = 25)
-clusters3 <- kmeans(cluster_statements_with_reduced_dimensions_spread, centers = 3, nstart = 25)
-clusters7 <- kmeans(cluster_statements_with_reduced_dimensions_spread, centers = 7, nstart = 25)
-clusters9 <- kmeans(cluster_statements_with_reduced_dimensions_spread, centers = 9, nstart = 25)
-clusters12 <- kmeans(cluster_statements_with_reduced_dimensions_spread, centers = 12, nstart = 25)
-
-fviz_nbclust(cluster_statements_with_reduced_dimensions_spread, kmeans, method = "wss")
-
-p1 <- fviz_cluster(clusters2, geom = "point", data = cluster_statements_with_reduced_dimensions_spread) + ggtitle("k = 2")
-p2 <- fviz_cluster(clusters3, geom = "point",  data = cluster_statements_with_reduced_dimensions_spread) + ggtitle("k = 3")
-p3 <- fviz_cluster(clusters7, geom = "point",  data = cluster_statements_with_reduced_dimensions_spread) + ggtitle("k = 7")
-p4 <- fviz_cluster(clusters12, geom = "point",  data = cluster_statements_with_reduced_dimensions_spread) + ggtitle("k = 12")
-
-library(gridExtra)
-grid.arrange(p1, p2, p3, p4, nrow = 2)
 
 
 

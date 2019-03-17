0bc12b172f88dfa165e96f4057125894f750fc8b
Lucas Cherkewski <lucas@ecustom.ca>
2019-03-17 16:32:16 -0400

    remove now-unused code

--- similarity.R
+++ similarity.R
@@ -24,42 +24,6 @@ base_statements <- qp_statements %>%
   inner_join(mps, by = c("member_id" = "id")) %>%
   mutate(year = year(time), week = week(time), year_week = as.integer(paste0(year, week)))
 
-## NB: Change the filter(s) as you see fit.
-## Examples: time, party, parliamentary session.
-statements_to_analyse <- base_statements %>%
-  filter(time > "2018-01-01")
-
-## Reduce the statements to lemmatized tokens. (Thanks Busa!)
-statements_by_token <- statements_to_analyse %>%
-  select(id, content_en_plaintext) %>%
-  unnest_tokens(word, content_en_plaintext) %>%
-  anti_join(stop_words) %>%
-  mutate(word = wordStem(word))
-
-## Calculate the TF-IDF score for each word.
-statements_by_word_frequency <- statements_by_token %>%
-  count(id, word, sort = TRUE) %>%
-  ungroup() %>%
-  bind_tf_idf(word, id, n)
-
-## Calculate the cosine similarity of each QP statement.
-statement_similarity <- statements_by_word_frequency %>%
-  do_cosine_sim.kv(subject = id, key = word, value = tf_idf, distinct = TRUE)
-
-## Retrieve the statements with the highest similarity scores.
-top_similar_statements <- statement_similarity %>%
-  filter(value > 0.5) %>%
-  top_n(100, value)
-
-## Just get unique IDs of documents that have high similarity scores.
-top_similar_statement_ids <- union(top_similar_statements$id.x, top_similar_statements$id.y)
-
-## View the statements with the highest scores.
-statements_to_analyse %>%
-  select(id, time, h2_en, who_en, short_name_en, content_en_plaintext) %>%
-  filter(id %in% top_similar_statement_ids) %>%
-  View()
-
 
 
 ## CLUSTERING
@@ -115,7 +79,7 @@ grid.arrange(p1, p2, p3, p4, nrow = 2)
 
 
 view_specific_statements(
-  statements_to_analyse,
+  base_statements,
   analyse_statement_similarity(
     base_statements %>%
       filter(time > "2018-12-01"),

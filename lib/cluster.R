baaac98ddfbd7e4c59e6e7cf25bb3c2592361862
Lucas Cherkewski <lucas@ecustom.ca>
2019-03-18 20:22:18 -0400

    expose a clusterer function?

--- lib/cluster.R
+++ lib/cluster.R
@@ -13,17 +13,22 @@ cluster_statements_kmeans <- function(statements_to_cluster) {
 
   optimal_cluster_count <- length(unique(optimal_cluster_count_summary$Best.partition))
 
-  clusters <- kmeans(
-    statements_with_reduced_dimensions_spread,
-    centers = optimal_cluster_count,
-    nstart = 25
-  )
+  clusterer <- function(cluster_count) {
+    kmeans(
+      statements_with_reduced_dimensions_spread,
+      centers = cluster_count,
+      nstart = 25
+    )
+  }
+  
+  optimal_clusters <- clusterer(optimal_cluster_count)
 
   clustered_statements <- statements_to_cluster %>%
-    mutate(cluster = clusters$cluster)
+    mutate(cluster = optimal_clusters$cluster)
 
   list(
     "scored_statements_by_id" = statements_with_reduced_dimensions_spread,
+    "clusterer" = clusterer,
     "clusters" = clusters,
     "cluster_count" = optimal_cluster_count,
     "clustered_statements" = clustered_statements
